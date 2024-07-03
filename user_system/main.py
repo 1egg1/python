46dbb30b96410d66f8482e8bbba53183adbcaeaf
Egg <152276256+1egg1@users.noreply.github.com>
2024-07-03 12:33:24 +0500

    Update main.py

--- user_system/main.py
+++ user_system/main.py
@@ -1,1 +1,15 @@
+import time
 
+users = []
+passwords = []
+
+current_user = ''
+
+def registration(pswrd, user_name):
+    users.append(user_name)
+    passwords.append(pswrd)
+
+while True:
+    command = input('1)Registraion\n2)Login\n3)Account\n4)Exit\n\ncommand: ')
+    if command == '1':
+        print('test')
